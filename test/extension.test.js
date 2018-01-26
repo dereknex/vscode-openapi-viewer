4923f950659cbf3d9ed5c56eb9bf5bbc65ade910
Derek Chen <derek@imoobox.com>
2018-01-26 19:45:00 +0800

    add test when ci

--- test/extension.test.js
+++ test/extension.test.js
@@ -17,8 +17,8 @@ const myExtension = require('../extension');
 suite("Extension Tests", function() {
 
     // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
-    });
+    // test("Something 1", function() {
+    //     assert.equal(-1, [1, 2, 3].indexOf(5));
+    //     assert.equal(-1, [1, 2, 3].indexOf(0));
+    // });
 });
